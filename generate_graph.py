af7a64e3d103c89f69b9a95d5d02fc8d24c68755
srini <21.srini@gmail.com>
2014-07-25 13:00:50 -0500

    Updated with graph generation

---
+++ generate_graph.py
@@ -0,0 +1,40 @@
+import networkx as nx,os,sys,math
+import matplotlib.pyplot as plt
+
+
+def usage():
+    print "num_nodes topology substar_count. if single star, 300 star 0 else 300 substar 5"
+    sys.exit(2)
+
+def generate_graph(num_nodes, topology, substar_count):
+    nodes=range(0,num_nodes)
+    G=nx.Graph()
+    G.add_nodes_from(nodes)
+    if(topology=='star'):
+        for node in range(0,num_nodes-1):
+            G.add_edge(node,num_nodes-1)
+        nx.draw(G)
+        plt.show()
+    elif(topology=='substar'):
+        subhead=range(substar_count-1,num_nodes,substar_count)
+        for a,b in zip(subhead,subhead[1:]):
+            G.add_edge(a,b)
+        for node in range(0,num_nodes):
+            G.add_edge(node, int(math.ceil(node/substar_count)*substar_count)+(substar_count-1))
+        print G.edges()
+        nx.draw(G)
+        plt.show()
+        
+
+
+def main():
+    if len(sys.argv)<2:
+        usage()
+    
+    num_nodes=int(sys.argv[1])
+    topology=sys.argv[2]
+    substar_count=int(sys.argv[3])
+    generate_graph(num_nodes,topology, substar_count)
+    
+if __name__ == '__main__':
+    main()
