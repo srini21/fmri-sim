2d28125ca863b16e61ae7817a9b7bf09c4b5ac37
Srinivasan Sivaramachandran <srini@Srinivasans-MacBook-Pro.local>
2014-09-01 01:00:44 -0500

    finished SVR

--- svm.py
+++ svm.py
@@ -1,5 +1,6 @@
-import sklearn.linear_model as lr
+from sklearn.svm import SVR
 import numpy as np
+from sklearn.metrics import mean_absolute_error,r2_score,mean_squared_error
 from sklearn.cross_validation import train_test_split
 
 Xd=np.load('concat_dti.npy')
@@ -20,30 +21,56 @@ Xf_train, Xf_test, yf_train, yf_test= train_test_split(Xf,yf,train_size=0.8,test
 
 Xf_est_train, Xf_est_test, yf_est_train, yf_est_test= train_test_split(Xf_est,yf_est,train_size=0.8,test_size=.2,random_state=0)
 
-lin=lr.LinearRegression()
+lin=SVR(kernel='rbf')
 
 print "true self"
 print "fmri"
-print lin.fit(Xf_train,yf_train).score(Xf_test,yf_test)
+test_size,phenotypes=yf_train.shape
+mae=[]
+mse=[]
+r2=[]
+for i in range(0,phenotypes):
+    yf_pred=lin.fit(Xf_train,yf_train[:,i]).predict(Xf_test)
+    mae.append(mean_absolute_error(yf_pred,yf_test[:,i]))
+    mse.append(mean_squared_error(yf_pred,yf_test[:,i]))
+    r2.append((r2_score(yf_test[:,i],lin.fit(Xf_train,yf_train[:,i]).predict(Xf_test))))
+print "mae:"+str(np.mean(mae))
+print "mse:"+str(np.mean(mse))
+print "r2:"+str(np.mean(r2))
+
 print "dti"
-print lin.fit(Xd_train,yd_train).score(Xd_test,yd_test)
+mae=[]
+mse=[]
 
-print "true est"
-print "fmri"
-print lin.fit(Xf_est_train,yf_est_train).score(Xf_est_test,yf_est_test)
-print "dti"
-print lin.fit(Xd_est_train,yd_est_train).score(Xd_est_test,yd_est_test)
+for i in range(0,phenotypes):
+    yd_pred=lin.fit(Xd_train,yd_train[:,i]).predict(Xd_test)
+    mae.append(mean_absolute_error(yd_pred,yd_test[:,i]))
+    mse.append(mean_squared_error(yd_pred,yd_test[:,i]))
+    r2.append((r2_score(yd_test[:,i],lin.fit(Xd_train,yd_train[:,i]).predict(Xd_test))))
+print "mae:"+str(np.mean(mae))
+print "mse:"+str(np.mean(mse))
+print "r2:"+str(np.mean(r2))
+print "fmri_w_comput"
+mae=[]
+mse=[]
 
-print "train in self and predict in est"
-print "fmri"
-print lin.fit(Xf_train,yf_train).score(Xf_est_test,yf_est_test)
-print "dti"
-print lin.fit(Xd_train,yd_train).score(Xd_est_test,yd_est_test)
+for i in range(0,phenotypes):
+    yf_est_pred=lin.fit(Xf_est_train,yf_est_train[:,i]).predict(Xf_est_test)
+    mae.append(mean_absolute_error(yf_est_pred,yf_est_test[:,i]))
+    mse.append(mean_squared_error(yf_est_pred,yf_est_test[:,i]))
+    r2.append((r2_score(yf_est_test[:,i],lin.fit(Xf_est_train,yf_est_train[:,i]).predict(Xf_est_test))))
+print "mae:"+str(np.mean(mae))
+print "mse:"+str(np.mean(mse))
+print "r2:"+str(np.mean(r2))
+print "dti_w_comput"
+mae=[]
+mse=[]
 
-
-print "train in est and predict in self"
-print "fmri"
-print lin.fit(Xf_est_train,yf_est_train).score(Xf_test,yf_test)
-print "dti"
-print lin.fit(Xd_est_train,yd_est_train).score(Xd_test,yd_test)
-
+for i in range(0,phenotypes):
+    yd_est_pred=lin.fit(Xd_est_train,yd_est_train[:,i]).predict(Xd_est_test)
+    mse.append(mean_squared_error(yd_est_pred,yd_est_test[:,i]))
+    mae.append(mean_absolute_error(yd_est_pred,yd_est_test[:,i]))
+    r2.append((r2_score(yd_est_test[:,i],lin.fit(Xd_est_train,yd_est_train[:,i]).predict(Xd_est_test))))
+print "mae:"+str(np.mean(mae))
+print "mse:"+str(np.mean(mse))
+print "r2:"+str(np.mean(r2))
